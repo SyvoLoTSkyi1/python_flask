8ff5c46f591c91046ccdf6d399e892df15d6803c
MykolaSyvolotskyi <nikolaysvl31@gmail.com>
2023-05-17 14:21:53 +0300

    create function get_filtered_customers

---
+++ flask_app_db/utils.py
@@ -0,0 +1,25 @@
+import os
+from typing import List
+
+from db_utils import execute_query
+
+
+def get_filtered_customers(city=None, state=None) -> List:
+    '''
+    Returns customers filtered by city and state
+     :param city: city
+    :param state: state
+    :return: client's list
+    '''
+    db_path = os.path.join(os.getcwd(), 'chinook.db')
+    query_sql = f'''
+        SELECT *
+          FROM customers
+    '''
+    if city and state:
+        query_sql += f"   WHERE City = '{city}' AND STATE = '{state}';"
+    elif city:
+        query_sql += f"   WHERE City = '{city}';"
+    elif state:
+        query_sql += f"   WHERE State = '{state}';"
+    return execute_query(query_sql, db_path)
